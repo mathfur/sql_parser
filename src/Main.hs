8863af1a541b8843b8ffc8f9e216441e9442b921
mathfuru <mathfuru@gmail.com>
2013-04-22 23:21:01 +0900

    Modify main (#32)

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,18 @@ import Parser (to_sql, for_test)
 import Formatter (format)
 import Type
 import ProcessSql
+import System.IO (getContents)
+import Control.Monad
+
+get_line_and_output :: IO ()
+get_line_and_output = do
+    sql <- getLine
+    case (Parser.to_sql sql) of
+      Right e -> do
+        let result = Formatter.format e
+        putStrLn $ "OK(" ++ show (length result) ++ "): " ++ show sql ++ ": " ++ show result
+      Left e -> do
+        putStrLn $ "NG: " ++ show sql
 
 main :: IO ()
-main = do
-  sql <- getContents
-  case (Parser.to_sql sql) of
-    Right e -> do
-      print e
-      print $ Formatter.format e
-    Left e -> do
-      print "(error)"
-      print e
+main = forever get_line_and_output
